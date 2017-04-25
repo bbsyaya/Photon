cdc05eda5452c41416a3366e254f123324de316d
Sam Symons <sam@samsymons.com>
2017-04-25 08:22:29 -0700

    Update the umbrella header license.

--- Framework/Photon/Photon.h
+++ Framework/Photon/Photon.h
@@ -1,10 +1,23 @@
+// Photon.h
+// Copyright (c) 2017 Sam Symons
 //
-//  Photon.h
-//  Photon
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Symons, Sam(AWF) on 25/04/17.
-//  Copyright © 2017 Sam Symons. All rights reserved.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <Cocoa/Cocoa.h>
 
@@ -13,7 +26,3 @@ FOUNDATION_EXPORT double PhotonVersionNumber;
 
 //! Project version string for Photon.
 FOUNDATION_EXPORT const unsigned char PhotonVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Photon/PublicHeader.h>
-
-
