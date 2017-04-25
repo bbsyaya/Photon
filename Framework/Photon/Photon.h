e860b43c5e9cc6446998b78d8246122514c1435e
Sam Symons <sam@samsymons.com>
2017-04-25 08:12:26 -0700

    Initial commit.

---
+++ Framework/Photon/Photon.h
@@ -0,0 +1,19 @@
+//
+//  Photon.h
+//  Photon
+//
+//  Created by Symons, Sam(AWF) on 25/04/17.
+//  Copyright © 2017 Sam Symons. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Photon.
+FOUNDATION_EXPORT double PhotonVersionNumber;
+
+//! Project version string for Photon.
+FOUNDATION_EXPORT const unsigned char PhotonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Photon/PublicHeader.h>
+
+
