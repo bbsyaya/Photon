0a848f52549308221f9b914e17aff3ef8bda4ea7
Sam Symons <sam@samsymons.com>
2017-04-29 18:06:29 -0700

    Add some stub classes and implement Point.swift.

---
+++ Framework/Photon/Math/Point.swift
@@ -0,0 +1,149 @@
+// Point.swift
+// Copyright (c) 2017 Sam Symons
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+
+// MARK: - Point2D
+
+public struct Point2D {
+  public static let zero = Point2D(0, 0)
+
+  var x: Float
+  var y: Float
+
+  public init(_ x: Float, _ y: Float) {
+    self.x = x
+    self.y = y
+  }
+}
+
+extension Point2D: Equatable {
+
+  // MARK: 2D Vector Arithmetic
+
+  public static func ==(lhs: Point2D, rhs: Point2D) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
+  }
+
+  public static func +(lhs: Point2D, rhs: Point2D) -> Point2D {
+    return Point2D(lhs.x + rhs.x, lhs.y + rhs.y)
+  }
+
+  public static func -(lhs: Point2D, rhs: Point2D) -> Point2D {
+    return Point2D(lhs.x - rhs.x, lhs.y - rhs.y)
+  }
+
+  public static func *(lhs: Point2D, rhs: Point2D) -> Point2D {
+    return Point2D(lhs.x * rhs.x, lhs.y * rhs.y)
+  }
+
+  public static func /(lhs: Point2D, rhs: Point2D) -> Point2D {
+    return Point2D(lhs.x / rhs.x, lhs.y / rhs.y)
+  }
+
+  // MARK: Float Arithmetic
+
+  public static func +(lhs: Point2D, rhs: Float) -> Point2D {
+    return Point2D(lhs.x + rhs, lhs.y + rhs)
+  }
+
+  public static func -(lhs: Point2D, rhs: Float) -> Point2D {
+    return Point2D(lhs.x - rhs, lhs.y - rhs)
+  }
+
+  public static func *(lhs: Point2D, rhs: Float) -> Point2D {
+    return Point2D(lhs.x * rhs, lhs.y * rhs)
+  }
+
+  public static func /(lhs: Point2D, rhs: Float) -> Point2D {
+    return Point2D(lhs.x / rhs, lhs.y / rhs)
+  }
+}
+
+extension Point2D: CustomStringConvertible {
+  public var description: String {
+    return "Point (x: \(x), y: \(y))"
+  }
+}
+
+
+// MARK: - Point3D
+
+public struct Point3D {
+  public static let zero = Point3D(0, 0, 0)
+
+  var x: Float
+  var y: Float
+  var z: Float
+
+  public init(_ x: Float, _ y: Float, _ z: Float) {
+    self.x = x
+    self.y = y
+    self.z = z
+  }
+}
+
+extension Point3D: Equatable {
+
+  public static func ==(lhs: Point3D, rhs: Point3D) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z
+  }
+
+  public static func +(lhs: Point3D, rhs: Point3D) -> Point3D {
+    return Point3D(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z)
+  }
+
+  public static func -(lhs: Point3D, rhs: Point3D) -> Point3D {
+    return Point3D(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z)
+  }
+
+  public static func *(lhs: Point3D, rhs: Point3D) -> Point3D {
+    return Point3D(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z)
+  }
+
+  public static func /(lhs: Point3D, rhs: Point3D) -> Point3D {
+    return Point3D(lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z)
+  }
+
+  // MARK: Float Arithmetic
+
+  public static func +(lhs: Point3D, rhs: Float) -> Point3D {
+    return Point3D(lhs.x + rhs, lhs.y + rhs, lhs.z + rhs)
+  }
+
+  public static func -(lhs: Point3D, rhs: Float) -> Point3D {
+    return Point3D(lhs.x - rhs, lhs.y - rhs, lhs.z - rhs)
+  }
+
+  public static func *(lhs: Point3D, rhs: Float) -> Point3D {
+    return Point3D(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs)
+  }
+
+  public static func /(lhs: Point3D, rhs: Float) -> Point3D {
+    return Point3D(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs)
+  }
+}
+
+extension Point3D: CustomStringConvertible {
+  public var description: String {
+    return "Point (x: \(x), y: \(y), z: \(z))"
+  }
+}
---
+++ Framework/Unit Tests/PointTests.swift
@@ -0,0 +1,164 @@
+// PointTests.swift
+// Copyright (c) 2017 Sam Symons
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+import XCTest
+
+
+// MARK: - Point2D
+
+final class Point2DTests: XCTestCase {
+
+  func testInitialization() {
+    let vector = Point2D(1.0, 2.0)
+
+    XCTAssertEqual(vector.x, 1.0)
+    XCTAssertEqual(vector.y, 2.0)
+  }
+
+  func testStaticHelperFunctions() {
+    XCTAssertEqual(Point2D.zero, Point2D(0.0, 0.0))
+  }
+
+  func testPointAddition() {
+    let v1 = Point2D(1.0, 2.0)
+    let v2 = Point2D(3.0, 4.0)
+
+    XCTAssertEqual(v1 + v2, Point2D(4.0, 6.0))
+  }
+
+  func testPointSubtraction() {
+    let v1 = Point2D(10.0, 5.0)
+    let v2 = Point2D(5.0, 5.0)
+
+    XCTAssertEqual(v1 - v2, Point2D(5.0, 0))
+  }
+
+  func testPointMultiplication() {
+    let v1 = Point2D(1.0, 2.0)
+    let v2 = Point2D(1.0, 3.0)
+
+    XCTAssertEqual(v1 * v2, Point2D(1.0, 6.0))
+  }
+
+  func testPointDivision() {
+    let v1 = Point2D(1.0, 3.0)
+    let v2 = Point2D(1.0, 2.0)
+
+    XCTAssertEqual(v1 / v2, Point2D(1.0, 1.5))
+  }
+
+  func testScalarAddition() {
+    let vector = Point2D(1.0, 2.0)
+
+    XCTAssertEqual(vector + 3, Point2D(4.0, 5.0))
+  }
+
+  func testScalarSubtraction() {
+    let vector = Point2D(1.0, 2.0)
+
+    XCTAssertEqual(vector - 1, Point2D(0, 1.0))
+  }
+
+  func testScalarMultiplication() {
+    let vector = Point2D(1.0, 2.0)
+
+    XCTAssertEqual(vector * 3, Point2D(3.0, 6.0))
+  }
+
+  func testScalarDivision() {
+    let vector = Point2D(1.0, 2.0)
+
+    XCTAssertEqual(vector / 2, Point2D(0.5, 1.0))
+  }
+
+}
+
+
+// MARK: - Point3D
+
+final class Point3DTests: XCTestCase {
+
+  func testInitialization() {
+    let vector = Point3D(1.0, 2.0, 3.0)
+
+    XCTAssertEqual(vector.x, 1.0)
+    XCTAssertEqual(vector.y, 2.0)
+    XCTAssertEqual(vector.z, 3.0)
+  }
+
+  func testStaticHelperFunctions() {
+    XCTAssertEqual(Point3D.zero, Point3D(0.0, 0.0, 0.0))
+  }
+
+  func testPointAddition() {
+    let v1 = Point3D(1.0, 2.0, 3.0)
+    let v2 = Point3D(4.0, 5.0, 6.0)
+
+    XCTAssertEqual(v1 + v2, Point3D(5.0, 7.0, 9.0))
+  }
+
+  func testPointSubtraction() {
+    let v1 = Point3D(10.0, 11.0, 12.0)
+    let v2 = Point3D(5.0, 6.0, 7.0)
+
+    XCTAssertEqual(v1 - v2, Point3D(5.0, 5.0, 5.0))
+  }
+
+  func testPointMultiplication() {
+    let v1 = Point3D(1.0, 2.0, 3.0)
+    let v2 = Point3D(1.0, 3.0, 0.0)
+
+    XCTAssertEqual(v1 * v2, Point3D(1.0, 6.0, 0.0))
+  }
+
+  func testPointDivision() {
+    let v1 = Point3D(1.0, 3.0, 10.0)
+    let v2 = Point3D(1.0, 2.0, 5.0)
+
+    XCTAssertEqual(v1 / v2, Point3D(1.0, 1.5, 2.0))
+  }
+
+  func testScalarAddition() {
+    let vector = Point3D(1.0, 2.0, 3.0)
+
+    XCTAssertEqual(vector + 3, Point3D(4.0, 5.0, 6.0))
+  }
+
+  func testScalarSubtraction() {
+    let vector = Point3D(1.0, 2.0, 3.0)
+
+    XCTAssertEqual(vector - 1, Point3D(0, 1.0, 2.0))
+  }
+
+  func testScalarMultiplication() {
+    let vector = Point3D(1.0, 2.0, 3.0)
+
+    XCTAssertEqual(vector * 3, Point3D(3.0, 6.0, 9.0))
+  }
+  
+  func testScalarDivision() {
+    let vector = Point3D(1.0, 2.0, 3.0)
+    
+    XCTAssertEqual(vector / 2, Point3D(0.5, 1.0, 1.5))
+  }
+  
+}
