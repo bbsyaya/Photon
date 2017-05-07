d996e46170d9c391ac1c546f196844aa9d6f3188
Sam Symons <sam@samsymons.com>
2017-05-07 12:20:33 -0700

    Fill out the documentation some more.

--- Photon.playground/Pages/Basic Ray Tracing.xcplaygroundpage/Contents.swift
+++ Photon.playground/Pages/Basic Ray Tracing.xcplaygroundpage/Contents.swift
@@ -1,16 +1,48 @@
-//: [Previous](@previous)
-
 import Foundation
 import Photon
 import XCPlayground
 
 XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+/*:
+ [Previous](@previous)
+ ****
+ # Basic Ray Tracing
 
-let scene = Scene(width: 300, height: 200, camera: OrthographicCamera())
+ Photon makes it easy to quickly get up and running with a simple
+ ray traced image. To begin, you create an instance of a `Scene`
+ object with the size and camera you need. Here, Photon is using
+ an orthographic camera, meaning rays are shot out parallel to
+ each other.
+ 
+ * Experiment:
+ Try using a `PerspectiveCamera` instance to get a more realistic
+ image. This renders objects the same way as the human eye would
+ perceive them, rather than the same size no matter the distance,
+ which is how an orthographic camera works.
+ */
 
-scene.add(object: Sphere(center: Point3D(300 / 2, 200 / 2, -50), radius: 60))
+let scene = Scene(width: 600, height: 400, camera: OrthographicCamera())
+
+/*:
+ Once you have a `Scene` object, you can add objects to it. Here,
+ a `Sphere` is created and added to the scene, with its center as
+ the middle of the scene.
+ */
+
+let sphereObject =  Sphere(center: Point3D(600 / 2, 400 / 2, -100), radius: 150)
+scene.add(object: sphereObject)
+
+/*:
+ Finally, once you have your scene with objects configured, call
+ the `renderScene` function to generate an image. Photon returns
+ an optional image here in case anything goes wrong with the
+ rendering process, so here we will forcefully unwrap the image
+ and hope for the best.
+ */
+
 scene.renderScene { image in
   let unwrappedImage = image!
 }
 
+//: ****
 //: [Next](@next)
