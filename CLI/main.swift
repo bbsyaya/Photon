81dca5fccfb671631658255bbe4618312e8eb645
Sam Symons <sam@samsymons.com>
2017-05-08 09:19:34 -0700

    Add an extremely crude main function for outputting a single image.
    This is 100% for quick testing and dabbling in performance improvements.

--- CLI/main.swift
+++ CLI/main.swift
@@ -23,3 +23,50 @@ import Foundation
 import Photon
 
 print("Photon 1.0")
+
+func getPath(fileName: String) -> String {
+  let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+  let folder = "PhotonRenders"
+
+  return documentURL.appendingPathComponent(folder).appendingPathComponent(fileName).path
+}
+
+extension NSImage {
+  @discardableResult func saveAsPNG(url: URL) -> Bool {
+    guard let tiffData = self.tiffRepresentation else {
+      print("failed to get tiffRepresentation. url: \(url)")
+      return false
+    }
+
+    let imageRep = NSBitmapImageRep(data: tiffData)
+
+    guard let imageData = imageRep?.representation(using: .PNG, properties: [:]) else {
+      print("failed to get PNG representation. url: \(url)")
+      return false
+    }
+
+    do {
+      try imageData.write(to: url)
+      return true
+    } catch {
+      print("failed to write to disk. url: \(url)")
+      return false
+    }
+  }
+}
+
+var renderingOptions = RenderingOptions(width: 600, height: 400)
+let scene = Scene(renderingOptions: renderingOptions)
+let sphereObject = Sphere(center: Point3D(600 / 2, 400 / 2, -100), radius: 150)
+let path = getPath(fileName: "TestRender.png")
+
+scene.add(object: sphereObject)
+
+scene.renderScene { image in
+  let unwrappedImage = image!
+  unwrappedImage.saveAsPNG(url: URL(fileURLWithPath: path))
+
+  exit(0)
+}
+
+dispatchMain()
